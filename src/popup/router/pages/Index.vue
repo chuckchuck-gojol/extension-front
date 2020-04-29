0a135a9763b95a3fc4ba3b0a237bb6555f9b38d4
mjung1798 <mor2222@naver.com>
2020-04-30 01:35:51 +0900

    Feat: change dir to vue.js

---
+++ src/popup/router/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p>Hello world! {{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'minjeong',
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
