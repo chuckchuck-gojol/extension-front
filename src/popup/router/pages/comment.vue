a01f842f3fc94dd5ad23ebf114bf6e4c5b1efc2c
mjung1798 <mor2222@naver.com>
2020-05-02 22:52:23 +0900

    Feat: complete user data binding

--- src/popup/router/pages/comment.vue
+++ src/popup/router/pages/comment.vue
@@ -1,16 +1,24 @@
 <template>
-    <div id="app">
-        <h3>comment</h3>
+    <div id="app">:
+        <p>{{user_channel}}</p>
+        <p>{{user_text}}</p>
+        <p>{{user_image}}</p>
+        <p>{{user_time}}</p>
+        <p>{{user_comment_good_count}}</p>
+        <div v-html="user_comment"></div>
     </div>
 </template>
 
 <script>
     export default {
+        props: ['user_channel','user_text', 'user_image', 'user_time', 'user_comment_good_count', 'user_comment'],
         data() {
             return {
-
             };
         },
+        mounted() {
+            console.log(this.user_channel)
+        }
     };
 </script>
 
